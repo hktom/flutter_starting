8dae74b0841120d7bc6e72e529f1af7c5d8a5933
tom hikari <tomprescott1565@gmail.com>
2022-07-25 14:19:33 +0100

    feature/state

--- lib/auth/bloc/auth_state.dart
+++ lib/auth/bloc/auth_state.dart
@@ -1,6 +1,37 @@
 part of 'auth_bloc.dart';
 
-@immutable
-abstract class AuthState {}
+class AuthState extends Equatable {
+  final bool? isAuthenticated;
+  final bool? isLoading;
+  final String? error;
+  final Map<String, String>? credentials;
 
-class AuthInitial extends AuthState {}
+  const AuthState({
+    this.isAuthenticated,
+    this.isLoading,
+    this.error,
+    this.credentials,
+  });
+
+  AuthState copyWith({
+    bool? isAuthenticated,
+    bool? isLoading,
+    String? error,
+    Map<String, String>? credentials,
+  }) {
+    return AuthState(
+      isAuthenticated: isAuthenticated ?? this.isAuthenticated,
+      isLoading: isLoading ?? this.isLoading,
+      error: error ?? this.error,
+      credentials: credentials ?? this.credentials,
+    );
+  }
+
+  @override
+  List get props => [
+        isAuthenticated,
+        isLoading,
+        error,
+        credentials,
+      ];
+}
